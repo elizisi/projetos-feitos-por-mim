2b8eb3793ef3e45a9f5bff70f20e2e4d64aa8a2e
Roger Elias <135875734+elizisi@users.noreply.github.com>
2023-07-17 22:09:47 -0300

    Add files via upload
    Cronômetro.

---
+++ Stopwatch/Program.cs
@@ -0,0 +1,66 @@
+﻿namespace Stopwatch;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Menu();
+
+    }
+
+    static void Menu()
+
+{
+    Console.Clear();
+    Console.WriteLine("S = Segundos => 10s = 10 Segundos.");
+    Console.WriteLine("M = Minutos => 1m = 1 Minuto.");
+    Console.WriteLine("0 = Sair.");
+    Console.WriteLine("Quanto tempo deseja contar?");
+
+    string data = Console.ReadLine().ToLower();
+    char type = char.Parse(data.Substring(data.Length -1, 1));
+    int time = int.Parse(data.Substring(0, data.Length -1));
+    int multiplier = 1;
+
+    if (type == 'm')
+       multiplier = 60;
+
+    if (time == 0)
+       System.Environment.Exit(0);
+
+       PreStart(time * multiplier);
+
+    
+}
+
+    static void PreStart(int time) 
+    {
+       Console.Clear();
+       Console.WriteLine("Preparando Cronômetro...");
+       Thread.Sleep(1000);
+       Console.WriteLine("Começando...");
+       Thread.Sleep(2100);
+
+       Start (time);
+
+    
+    }
+
+    static void Start(int time) 
+    {
+       int currentTime = 0;
+
+       while(currentTime != time)
+       {
+         Console.Clear();
+         currentTime++;
+         Console.WriteLine(currentTime);
+         Thread.Sleep(1000);
+       }
+
+       Console.Clear();
+       Console.WriteLine("Stopwatch finalizado!");
+       Thread.Sleep(1000);
+       Menu();
+    }
+}
