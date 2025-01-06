fb35a3125bce423519fafe583705fcb81cf63176
Roger Elias <135875734+elizisi@users.noreply.github.com>
2025-01-06 19:03:08 -0300

    Add files via upload

---
+++ Permissão para Dirigir/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Permissão_para_Dirigir_
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("digite sua idade:");
+            int idade = int.Parse(Console.ReadLine());
+            bool temHabilitacao = true;
+
+            if (idade >= 18 && temHabilitacao)
+            {
+                Console.WriteLine("Parabéns, você pode dirigir! ");
+            }
+            else
+            {
+                Console.WriteLine("Desculpe, você não pode dirigir.");
+            }
+        }
+    }
+}
